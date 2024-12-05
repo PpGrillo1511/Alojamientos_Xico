6b568eaaa3be74097e9435f8609b9396595e5701
jareni gomez juan <jarenigomezjuan@gmail.com>
2024-12-05 13:02:56 -0600

    Vistas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -7,4 +7,21 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Alojamientos Xico" });
 });
 
+// Nueva ruta para el diseño diferente cuando se hace clic en "Inicio"
+router.get("/inicio", (req, res) => {
+  res.render("inicio", { title: "Diseño Diferente de Inicio" });
+});
+
+router.get("/rentas", (req, res) => {
+  res.render("rentas", { title: "rentas" });
+});
+
+router.get("/sugerencias", (req, res) => {
+  res.render("sugerencias", { title: "suge" });
+});
+
+router.get("/resenas", (req, res) => {
+  res.render("resenas", { title: "rese" });
+});
+
 export default router;
